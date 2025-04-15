0036e412201df31d77f4d6764e60c2f5e404861b
Vineetpal4682 <vineetpal4682@gmail.com>
2025-04-15 11:51:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Firstform from './components/Firstform';
+import Footer from './components/Footer';
+import About from './components/About';
+import React,{useState} from 'react'
+import Alert from './components/Alert';
+
+// import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+
+let name= "vineet";
+function App() 
+{
+  const [mode, setMode] = useState('light');
+  const [alertmessage, setalertmessage] = useState(null);
+  const setAlert=(message,type)=>
+    {
+      setalertmessage({
+        msg:message,
+        type: type
+      })
+
+      setTimeout(() => {
+        setalertmessage(null);
+      }, 1000);    
+    }
+
+    const toggelModef=()=>
+      {
+        if(mode === 'light')
+          {
+            setMode('dark');
+            setAlert('Dark mode has been enabled!!','success'); 
+            document.title = "Dark Mode Enabled !!";            
+          }
+        else
+        {
+          setMode('light');
+          setAlert('Light mode has been enabled!!','success');
+          document.title = "Light Mode Enabled !!"; 
+        }
+      }
+  return (
+<>
+{/* <Router> */}
+  <Navbar tname="My First App" mode={mode} toggelMode={toggelModef}/>
+  <Alert alert={alertmessage}/>
+  <div className="container">   
+  <Firstform textHeadding = "Enter The Text" setAlert={setAlert}/>
+    {/* <Routes>
+    <Route path="/" element={<Firstform textHeadding = "Enter The Text" setAlert={setAlert}/>} />
+    <Route path="/about" element={<About title='About Us'/>} />
+    </Routes> */}
+  </div>
+  <Footer/>
+{/* </Router> */}
+</>
+    );
+}
+
+export default App;
---
+++ src/components/Firstform.js
@@ -0,0 +1,54 @@
+import React,{useState} from 'react'
+
+
+export default function Firstform(props) 
+{
+  const [textVar, setTextVar] = useState('');
+
+  const makeup =()=> {
+    console.log('makeup');  
+        setTextVar(textVar.toLocaleUpperCase());  
+        props.setAlert("Converted to Upper case.","success");  
+    }
+  const makeClear =(event)=> {
+      console.log('makeuptext'); 
+       setTextVar("");    
+       props.setAlert("All text clear!!","success");  
+    }
+
+  const makeLower =()=> {
+      console.log('makeup');  
+          setTextVar(textVar.toLocaleLowerCase());
+          props.setAlert("Converted to Lower case.","success");     
+      }
+
+  const makeuptext =(event)=> {
+        console.log('makeuptext'); 
+         setTextVar(event.target.value); 
+           
+      }
+    
+  return (
+    <>
+  <div className="container">     
+      <h1>{props.textHeadding}</h1>
+      <div className="input-group">
+          <span className="input-group-text">With textarea</span>
+          <textarea onChange={makeuptext} className="form-control" aria-label="With textarea" id="myText" rows="8" value={textVar}></textarea> 
+      </div>  
+      <button className="btn btn-primary mx-1" onClick={makeup}>Convert Upper</button>  
+      <button className="btn btn-primary mx-1" onClick={makeLower}>Convert Lower</button>
+      <button className="btn btn-primary mx-1" onClick={makeClear}>Convert Lower</button>  
+  </div>
+ <div className="container my-5">
+  <h1>Type Summary</h1>
+  <p>{textVar.split(" ").length} words and {textVar.length} characters</p>
+  <p>{textVar.split(" ").length*0.008} minutes to taken in reading...</p>
+<h2>Preview</h2>
+  <p>{textVar.length> 0 ?textVar:'Enter Somthing above to preview here!!!' }</p>
+
+ </div>
+
+</>
+  )
+}
